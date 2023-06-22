9852020c58916d88e09e4dfdb0b2cb12f6fa6990
YashIngole <yashingole1000@gmail.com>
2023-06-22 20:17:38 +0530

    added form key

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,6 +1,5 @@
-import 'package:cab_management/Signin.dart';
-import 'package:cab_management/pallete.dart';
 import 'package:flutter/material.dart';
+import 'package:cab_management/pallete.dart';
 import 'constants.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,232 +14,234 @@ class _LoginPageState extends State<LoginPage> {
 
   var email = "";
   var password = "";
-  var confirmpassword = "";
-
-  //here are email and password conrtrollers
+  var confirmPassword = "";
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  final confirmpasswordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
 
   @override
   void dispose() {
     emailController.dispose();
     passwordController.dispose();
-    confirmpasswordController.dispose();
+    confirmPasswordController.dispose();
     super.dispose();
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        key: _formKey,
-        child: Column(children: [
-          Image.asset(""),
-          Padding(
-            padding: const EdgeInsets.only(top: 250),
-            child: Center(
-              child: const Text(
-                'Sign Up.',
-                style: TextStyle(
-                    fontSize: 45,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 7, 7, 7)),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 350, maxHeight: 60),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Name',
-                  labelStyle: TextStyle(color: Colors.black),
-                  hintStyle: TextStyle(
-                      fontSize: 20.0,
-                      color: Color.fromARGB(255, 110, 109, 109)),
-                  contentPadding: EdgeInsets.all(20),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 104, 104, 105),
-                        width: 3,
-                      ),
-                      borderRadius: BorderRadius.circular(12)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 0, 0, 5),
-                        width: 3,
-                      ),
-                      borderRadius: BorderRadius.circular(12)),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 3),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 350, maxHeight: 60),
-              child: TextFormField(
-                controller: emailController,
-                /*validator: (val) {
-                  return RegExp(
-                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                          .hasMatch(val!)
-                      ? null
-                      : "Please enter a valid email";
-                },*/
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.black),
-                  focusColor: Colors.black,
-                  hintStyle: TextStyle(
-                      fontSize: 20.0,
-                      color: Color.fromARGB(255, 110, 109, 109)),
-                  contentPadding: EdgeInsets.all(20),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 104, 104, 105),
-                        width: 3,
-                      ),
-                      borderRadius: BorderRadius.circular(12)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 0, 0, 5),
-                        width: 3,
-                      ),
-                      borderRadius: BorderRadius.circular(12)),
-                ),
-
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please Enter Email';
-                  } else if (!value.contains('@')) {
-                    return 'Please Enter Valid Email';
-                  }
-                  return null;
-                }, //if else statement for email @
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 23),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 350, maxHeight: 60),
-              child: TextFormField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Create Password',
-                  labelStyle: TextStyle(color: Colors.black),
-                  hintStyle: TextStyle(
-                      fontSize: 20.0,
-                      color: Color.fromARGB(255, 110, 109, 109)),
-                  contentPadding: EdgeInsets.all(20),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 104, 104, 105),
-                        width: 3,
-                      ),
-                      borderRadius: BorderRadius.circular(12)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 0, 0, 5),
-                        width: 3,
-                      ),
-                      borderRadius: BorderRadius.circular(12)),
-                ),
-
-                //validation of password
-
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please Enter Password';
-                  }
-                  return null;
-                },
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 23),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 350, maxHeight: 60),
-              child: TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Confirm Password',
-                  labelStyle: TextStyle(color: Colors.black),
-                  hintStyle: TextStyle(
-                      fontSize: 20.0,
-                      color: Color.fromARGB(255, 110, 109, 109)),
-                  contentPadding: EdgeInsets.all(20),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 104, 104, 105),
-                        width: 3,
-                      ),
-                      borderRadius: BorderRadius.circular(12)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 0, 0, 5),
-                        width: 3,
-                      ),
-                      borderRadius: BorderRadius.circular(12)),
-                ),
-
-                //confirm password validator
-
-                controller: confirmpasswordController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Password Mismatched';
-                  }
-                  return null;
-                },
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 23),
-            child: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Pallete.gradient1,
-                      Pallete.gradient2,
-                      Pallete.gradient3
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(12)),
-              child: ElevatedButton(
-                onPressed: () {
-                  //Validating password and email by register button
-
-                  if (_formKey.currentState!.validate()) {
-                    setState(() {
-                      email = emailController.text;
-                      password = passwordController.text;
-                      confirmpassword = confirmpasswordController.text;
-                    });
-                  }
-                },
-                child: Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(""),
+              Padding(
+                padding: const EdgeInsets.only(top: 250),
+                child: Center(
+                  child: const Text(
+                    'Sign Up.',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 7, 7, 7),
+                    ),
                   ),
                 ),
-                style: ElevatedButton.styleFrom(
-                    fixedSize: Size(350, 50),
-                    backgroundColor: Colors.transparent,
-                    shadowColor: Colors.transparent),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 350, maxHeight: 60),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintStyle: TextStyle(
+                        fontSize: 20.0,
+                        color: Color.fromARGB(255, 110, 109, 109),
+                      ),
+                      contentPadding: EdgeInsets.all(20),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 104, 104, 105),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 0, 0, 5),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 3),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 350, maxHeight: 60),
+                  child: TextFormField(
+                    controller: emailController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an email';
+                      } else if (!value.contains('@')) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Colors.black),
+                      focusColor: Colors.black,
+                      hintStyle: TextStyle(
+                        fontSize: 20.0,
+                        color: Color.fromARGB(255, 110, 109, 109),
+                      ),
+                      contentPadding: EdgeInsets.all(20),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 104, 104, 105),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 0, 0, 5),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 23),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 350, maxHeight: 60),
+                  child: TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Create Password',
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintStyle: TextStyle(
+                        fontSize: 20.0,
+                        color: Color.fromARGB(255, 110, 109, 109),
+                      ),
+                      contentPadding: EdgeInsets.all(20),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 104, 104, 105),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 0, 0, 5),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 23),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 350, maxHeight: 60),
+                  child: TextFormField(
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please confirm your password';
+                      } else if (value != passwordController.text) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintStyle: TextStyle(
+                        fontSize: 20.0,
+                        color: Color.fromARGB(255, 110, 109, 109),
+                      ),
+                      contentPadding: EdgeInsets.all(20),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 104, 104, 105),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 0, 0, 5),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 23),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Pallete.gradient1,
+                        Pallete.gradient2,
+                        Pallete.gradient3,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        setState(() {
+                          email = emailController.text;
+                          password = passwordController.text;
+                          confirmPassword = confirmPasswordController.text;
+                        });
+                      }
+                    },
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(350, 50),
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ]),
+        ),
       ),
     );
   }
