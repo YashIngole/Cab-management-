24b109ffcb665c9e427e34ed82900de3b759a472
Harsh <harshrajurkar201@gmail.com>
2023-06-27 01:05:44 +0530

    added two dialouge box

--- lib/Cab/cabPage.dart
+++ lib/Cab/cabPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'database.dart';
-import 'package:cab_management/home.dart';
+import 'database_c.dart';
 
 class cabPage extends StatefulWidget {
   @override
@@ -8,13 +7,65 @@ class cabPage extends StatefulWidget {
 }
 
 class _cabPageState extends State<cabPage> {
-  // const cabPage({super.key});
-  
+  String C_name = "";
+  String C_id = "";
+  String C_type = "";
+  String C_RTO = "";
+  final Database_c database_c = Database_c();
+  Stream? Cabs;
+  String ImageUrl = "";
+
+  late PageController _myPage;
+  var selectedPage;
+  String inputValue = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _myPage = PageController(initialPage: 0);
+    selectedPage = 0;
+  }
+  void _onItemTapped(int index) {
+    setState(() {
+      selectedPage = index;
+      _myPage.animateToPage(index,
+          duration: Duration(milliseconds: 500), curve: Curves.easeOut);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addNewCabPopUp(context);
+        },
+        child: Icon(Icons.add, color: Colors.white),
+      ),
     );
+  }
+  addNewCabPopUp(BuildContext context  ){
+    showDialog(context: context, builder: (context) {
+      return StatefulBuilder(builder: (context, setState) {
+        return AlertDialog(
+          title: const Text(
+              "Add Cab",
+              textAlign: TextAlign.center,
+            ),
+        );
+      });
+    });
+  }
+  addNewDriverPopUp(BuildContext context  ){
+    showDialog(context: context, builder: (context) {
+      return StatefulBuilder(builder: (context, setState) {
+        return AlertDialog(
+          title: const Text(
+              "Add Driver",
+              textAlign: TextAlign.center,
+            ),
+        );
+      });
+    });
   }
 }
