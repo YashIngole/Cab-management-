ef0bf7119ba0fdad65a8922d9b8e93121f9024e6
Harsh <harshrajurkar201@gmail.com>
2023-06-26 23:49:45 +0530

    starting with new UI

--- lib/Cab/cabPage.dart
+++ lib/Cab/cabPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'database.dart';
+import 'package:cab_management/home.dart';
 
 class cabPage extends StatefulWidget {
   @override
@@ -7,49 +9,12 @@ class cabPage extends StatefulWidget {
 
 class _cabPageState extends State<cabPage> {
   // const cabPage({super.key});
-  Icon custicon = Icon(Icons.search);
-
-  Widget custappbar = Text("Cabs");
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.only(left: 150),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 100,
-            width: 200,
-            color: Colors.grey,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 100,
-            width: 200,
-            color: Colors.grey,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 100,
-            width: 200,
-            color: Colors.grey,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 100,
-            width: 200,
-            color: Colors.grey,
-          )
-        ],
-      ),
-    ));
+
+
+    );
   }
 }
