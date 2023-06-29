5de2340a06c8b14b7b6e0f2c1d472ac4c0fe8680
Harsh <harshrajurkar201@gmail.com>
2023-06-29 23:14:38 +0530

    .

--- lib/Cab/updatecab.dart
+++ lib/Cab/updatecab.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:cab_management/constants.dart';
 import 'package:cab_management/firebase_options.dart';
 import 'package:cab_management/main.dart';
@@ -50,9 +53,9 @@ class _UpdateCabPageState extends State<UpdateCabPage> {
   late String newNameValue;
   late String newcabtypeValue;
   late String newcabRTOValue;
-  
+
   late String ImageUrl;
-  
+
   //Object? ImageUrl;
 
   @override
@@ -65,6 +68,11 @@ class _UpdateCabPageState extends State<UpdateCabPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Stack(
+              children: [
+                CircleAvatar()
+              ],
+            ),
             Center(
               child: Padding(
                 padding: const EdgeInsets.all(20),
@@ -75,47 +83,49 @@ class _UpdateCabPageState extends State<UpdateCabPage> {
                     color: Colors.black,
                     borderRadius: BorderRadius.circular(1000),
                   ),
-                  child:  InkWell(
+                  child: InkWell(
                     child: Icon(
                       Icons.add_a_photo,
                       color: Colors.white,
                       size: 50,
                     ),
-                    onTap: () async {
-                              ImagePicker imagePicker = ImagePicker();
-                              XFile? file = await imagePicker.pickImage(
-                                source: ImageSource.gallery,
-                              );
-                              if (file == null) {
-                                return;
-                              }
-                              final Uint8List fileBytes =
-                                  await file.readAsBytes();
+                    // onTap: () async {
+                    //   ImagePicker imagePicker = ImagePicker();
+                    //   XFile? file = await imagePicker.pickImage(
+                    //     source: ImageSource.gallery,
+                    //   );
+                    //   if (file == null) {
+                    //     return;
+                    //   }
+                    //   final Uint8List fileBytes = await file.readAsBytes();
 
-                              Reference referenceRoot =
-                                  FirebaseStorage.instance.ref();
-                              Reference referenceDirImages =
-                                  referenceRoot.child('images');
+                    //   Reference referenceRoot = FirebaseStorage.instance.ref();
+                    //   Reference referenceDirImages =
+                    //       referenceRoot.child('images');
 
-                              String uniqueFileName = DateTime.now()
-                                      .millisecondsSinceEpoch
-                                      .toString() +
-                                  '.jpg';
-                              Reference referenceImageToUpload =
-                                  referenceDirImages.child(uniqueFileName);
-                              try {
-                                await referenceImageToUpload.putData(
-                                    fileBytes as Uint8List,
-                                    SettableMetadata(
-                                        contentType: 'image/jpeg'));
-                                ImageUrl = await referenceImageToUpload
-                                    .getDownloadURL();
-                                print(ImageUrl);
-                              } catch (e) {
-                                print('Error uploading image: $e');
-                              }
-                            },
-                    
+                    //   String uniqueFileName =
+                    //       DateTime.now().millisecondsSinceEpoch.toString() +
+                    //           '.jpg';
+                    //   Reference referenceImageToUpload =
+                    //       referenceDirImages.child(uniqueFileName);
+                    //   try {
+                    //     await referenceImageToUpload.putData(
+                    //         fileBytes as Uint8List,
+                    //         SettableMetadata(contentType: 'image/jpeg'));
+                    //     ImageUrl =
+                    //         await referenceImageToUpload.getDownloadURL();
+                    //     print(ImageUrl);
+                    //   } catch (e) {
+                    //     print('Error uploading image: $e');
+                    //   }
+                    //   Future<Image> convertFileToImage(File picture) async {
+                    //     List<int> imageBase64 = picture.readAsBytesSync();
+                    //     String imageAsString = base64Encode(imageBase64);
+                    //     Uint8List uint8list = base64.decode(imageAsString);
+                    //     Image image = Image.memory(uint8list);
+                    //     return image;
+                    //   }
+                    // },
                   ),
                 ),
               ),
